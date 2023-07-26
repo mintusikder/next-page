b9ec5bfb20d0ecd84f9f4cee3868596307e6452a
Mintu Sikder <mintu141617@gmail.com>
2023-07-26 17:41:41 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./component/Home.jsx";
+import About from "./component/About.jsx";
+import Books from "./component/Books.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/books",
+        element: <Books/>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
