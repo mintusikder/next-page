312c32d5e1f40481e5f44f76b9b71541faf646b3
Mintu Sikder <mintu141617@gmail.com>
2023-07-26 19:24:02 +0600

    added header

--- src/component/Books.jsx
+++ src/component/Books.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Books = () => {
     return (
         <div>
-            
+            <h3>All Books coming</h3>
         </div>
     );
 };
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div  >
             <h3>Home Page</h3>
         </div>
     );
